6e056ba77122069ce0fa589f747dc29d1273e086
Max Hawkins <maxhawkins@gmail.com>
2017-05-09 08:06:13 -0700

    add some cors headers

--- server.py
+++ server.py
@@ -9,6 +9,16 @@ import evaluator
 import routing
 
 class RouteHandler(tornado.web.RequestHandler):
+
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, content-type")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
     def post(self):
         req = json.loads(self.request.body)
         nodes = req['nodes']
